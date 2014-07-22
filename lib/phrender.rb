d1e72d38c1d649c433c415b73c0e0cb0a5e674fa
M Smart <mina.smart@thescore.com>
2014-07-22 17:07:16 -0400

    Finish removing old junk.

--- lib/phrender.rb
+++ lib/phrender.rb
@@ -1,61 +1,10 @@
 require "phrender/version"
-require "phrender/drivers"
-require "phrender/engine"
-require "phrender/site"
-require "phrender/static_middleware"
-
-require "rack"
+require "phrender/logger"
+require "phrender/phantom_js_engine"
+require "phrender/phantom_js_session"
+require "phrender/rack_base"
+require "phrender/rack_middleware"
+require "phrender/rack_static"
 
 class Phrender
-  EMBER_DRIVER = File.read(
-    File.expand_path('../phrender/support/ember_driver.js', __FILE__))
-
-  attr_reader :site
-  attr_reader :engine
-
-  def initialize(site, opts = {})
-    timeout = opts[:timeout] || 10000
-    @site = site
-    site.save_source
-    engine_opts = {
-      timeout: timeout,
-      index_file: @site.index_file,
-      script: @site.source_file_path
-    }
-    @engine = Phrender::Engine.new engine_opts
-    at_exit do
-      cleanup
-    end
-  end
-
-  def app
-    app = self
-    Rack::Builder.new do
-      use Rack::CommonLogger
-      use Phrender::StaticMiddleware
-      run app
-    end
-  end
-
-  def cleanup
-    puts "Cleaning up..."
-    @site.dispose
-  end
-
-  def call(env)
-    request = Rack::Request.new env
-    path = request.path_info
-    host = request.host
-
-    url = "http://#{host}#{path}"
-    html = @engine.render url
-
-    response = Rack::Response.new
-    response.status = 200
-    response['Content-Type'] = 'text/html'
-    response.write html
-    response.finish
-  end
-
 end
-
---
+++ lib/phrender/ember_driver.rb
@@ -0,0 +1,4 @@
+class Phrender
+  EMBER_DRIVER = File.read(
+    File.expand_path('../phrender/support/ember_driver.js', __FILE__))
+end
